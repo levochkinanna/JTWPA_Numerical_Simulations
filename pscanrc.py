627b6c7f8897bc78699fbde2768069669cede832
levochkinanna <129070231+levochkinanna@users.noreply.github.com>
2023-09-03 21:48:21 +0200

    Add files via upload

---
+++ pscanrc.py
@@ -0,0 +1,4 @@
+psglobals.OptimizeSimulation = False
+SimulationParameters.DTMax = 0.5
+SimulationParameters.DTMin = 0.25
+psglobals.DimUnits = {'mA':0.001, 'mV':1, 'Ohm':1000.0, 'pH':329, 'ps':0.329, 'fF':0.329, 'fWb':0.329 }
