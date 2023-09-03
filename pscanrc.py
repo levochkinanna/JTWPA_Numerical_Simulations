74e7efcf34cf3195ba920d4b24f4bf6757327210
levochkinanna <129070231+levochkinanna@users.noreply.github.com>
2023-09-03 21:58:02 +0200

    Delete pscanrc.py

--- pscanrc.py
+++
@@ -1,4 +0,0 @@
-psglobals.OptimizeSimulation = False
-SimulationParameters.DTMax = 0.5
-SimulationParameters.DTMin = 0.25
-psglobals.DimUnits = {'mA':0.001, 'mV':1, 'Ohm':1000.0, 'pH':329, 'ps':0.329, 'fF':0.329, 'fWb':0.329 }
