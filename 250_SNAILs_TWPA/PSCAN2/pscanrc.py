943357c09c83855cf156e136885a5ab7927e5078
levochkinanna <129070231+levochkinanna@users.noreply.github.com>
2023-12-11 18:42:09 +0100

    Add files via upload

---
+++ 250_SNAILs_TWPA/PSCAN2/pscanrc.py
@@ -0,0 +1,6 @@
+# Here you can set units for further normalisation
+# Pay attantion magnetic flux quantum is already set as 2π 
+psglobals.OptimizeSimulation = False
+SimulationParameters.DTMax = 1
+SimulationParameters.DTMin = 1
+psglobals.DimUnits = {'mA':0.001, 'mV':1, 'Ohm':1000.0, 'pH':329, 'ps':0.329, 'fF':0.329, 'fWb':0.329 }
